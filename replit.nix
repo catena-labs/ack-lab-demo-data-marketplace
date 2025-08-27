3ac73e68404f3f5e101d50a5f9ff2d2e7c548b2d
Dominic Bosco <dominic@catenalabs.com>
2025-08-27 15:53:57 -0400

    node 20

--- replit.nix
+++ replit.nix
@@ -1,10 +1,6 @@
 { pkgs }: {
   deps = [
-    # Prefer the newest Node available on your channel.
-    # Try 22 first; if your channel doesn't have it, switch to 20.
-    pkgs.nodejs-22_x
-
-    # pnpm CLI
+    pkgs.nodejs-20_x
     pkgs.nodePackages.pnpm
   ];
 }
